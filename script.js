e4d7e27c78557fcdf1ad218d90aceba0833aea92
testddoser <129606911+testddoser@users.noreply.github.com>
2023-09-08 17:22:34 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$('form').on('submit',function(e){
-  e.preventDefault();
-  var but = $(this).find('[type="submit"]').toggleClass('sending').blur();
-  
-  setTimeout(function(){
-     but.removeClass('sending').blur();
-  },4500);
-  
-})
